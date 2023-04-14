22c5628bb5620013c389dad83e3eec1889a20e83
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-04-14 11:36:53 +0530

    fnalfnaf

--- src/components/services/auth/signUp.jsx
+++ src/components/services/auth/signUp.jsx
@@ -1,4 +1,4 @@
-import {auth,db} from './firebase';
+import {auth,db} from '../firebase';
 import { doc, setDoc } from 'firebase/firestore';
 import {GoogleAuthProvider,signInWithPopup, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import {useNavigate} from "react-router-dom";
