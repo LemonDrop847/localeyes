af5d48d209b576ae37e54bff0ec610d79002071c
LemonDrop847 <nitinmishra847@gmail.com>
2023-04-14 12:26:24 +0530

    Added functions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Home from './components/pages/homePage';
 import Feed from './components/pages/feedPage';
+import About from './components/pages/aboutPage';
+import Profile from './components/pages/profilepage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/feed" element={<Feed/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/profile" element={<Profile/>}/>
         </Routes>
       </Router>
     </div>
