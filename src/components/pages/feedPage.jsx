d269d088ac62af6da1193904c2fdc330fb0c711d
Soyamprabha <mallick.soyam@gmail.com>
2023-04-14 19:58:55 +0530

    home page

--- src/components/pages/feedPage.jsx
+++ src/components/pages/feedPage.jsx
@@ -1,7 +1,7 @@
 import'./styles/feedpage.css'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Feed = () => {
     return (  
         <> 
