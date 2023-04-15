986fdc62d12a217d34810bfeae3622837ade080a
LemonDrop847 <nitinmishra847@gmail.com>
2023-04-15 10:41:31 +0530

    changes

--- src/components/pages/aboutPage.jsx
+++ src/components/pages/aboutPage.jsx
@@ -1,76 +1,73 @@
-import'./styles/aboutpage.css'
+import "./styles/aboutpage.css";
 
 const About = () => {
   return (
-   <>
-   <h1> COMMUNITY-LED, PEOPLE-POWERED. </h1>
-   <div className="aboutusContent">
-    <p> Small in size, but mighty in purpose. LocalEyes is a team of determined problem-solvers who are aim to address issues at their core. Whether it's a problem with infrastructure, environmental concerns, or community safety, LocalEyes is here to support users every step of the way. We believe that everyone has a role to play in creating positive change in their communities, and we are committed to helping every citizen achieve that goal. We’ve worked hard to build a company, where we do big things. We’re here to help you do the same. 
-    </p>  
-   </div>   
-   <h2> THE TEAM </h2>
+    <>
+      <h1> COMMUNITY-LED, PEOPLE-POWERED. </h1>
+      <div className="aboutusContent">
+        <p>
+          {" "}
+          Small in size, but mighty in purpose. LocalEyes is a team of
+          determined problem-solvers who are aim to address issues at their
+          core. Whether it's a problem with infrastructure, environmental
+          concerns, or community safety, LocalEyes is here to support users
+          every step of the way. We believe that everyone has a role to play in
+          creating positive change in their communities, and we are committed to
+          helping every citizen achieve that goal. We’ve worked hard to build a
+          company, where we do big things. We’re here to help you do the same.
+        </p>
+      </div>
+      <h2> THE TEAM </h2>
       <div className="teamContainer">
         <div className="cards">
-        <div className="content">
-          <div className="imgBox">
-            <img src='https://i.postimg.cc/tRrFP53M/IMG-20230414-165917.jpg'/>
-          </div>
-          <div className="contentBox">
-            <h4>
-              Nitin Mishra
-            </h4>
-          </div>
+          <div className="content">
+            <div className="imgBox">
+              <img src="https://i.postimg.cc/tRrFP53M/IMG-20230414-165917.jpg" />
+            </div>
+            <div className="contentBox">
+              <h4>Nitin Mishra</h4>
+            </div>
           </div>
         </div>
       </div>
       <div className="teamContainer">
         <div className="cards">
-        <div className="content">
-          <div className="imgBox">
-            <img src=' https://i.postimg.cc/MHh7B6CY/anil.jpg'/>
-          </div>
-          <div className="contentBox">
-            <h4>
-              Anil Kumar Behera
-            </h4>
-          </div>
+          <div className="content">
+            <div className="imgBox">
+              <img src=" https://i.postimg.cc/MHh7B6CY/anil.jpg" />
+            </div>
+            <div className="contentBox">
+              <h4>Anil Kumar Behera</h4>
+            </div>
           </div>
         </div>
       </div>
       <div className="teamContainer">
         <div className="cards">
-        <div className="content">
-          <div className="imgBox">
-            <img src='https://i.postimg.cc/7ZzBmcX6/meeee.jpg'/>
-          </div>
-          <div className="contentBox">
-            <h4>
-              Srusti Prusty
-            </h4>
-          </div>
+          <div className="content">
+            <div className="imgBox">
+              <img src="https://i.postimg.cc/7ZzBmcX6/meeee.jpg" />
+            </div>
+            <div className="contentBox">
+              <h4>Srusti Prusty</h4>
+            </div>
           </div>
         </div>
       </div>
       <div className="teamContainer">
         <div className="cards">
-        <div className="content">
-          <div className="imgBox">
-            <img src='https://i.postimg.cc/3RKQMpxP/IMG-20230304-104403-1.jpg'/>
-          </div>
-          <div className="contentBox">
-            <h4>
-              Soyam Prabha Mallick
-            </h4>
-          </div>
+          <div className="content">
+            <div className="imgBox">
+              <img src="https://i.postimg.cc/3RKQMpxP/IMG-20230304-104403-1.jpg" />
+            </div>
+            <div className="contentBox">
+              <h4>Soyam Prabha Mallick</h4>
+            </div>
           </div>
         </div>
-
-      
       </div>
-  
-   </>
-
+    </>
   );
-}
+};
 
 export default About;
--- src/components/pages/homePage.jsx
+++ src/components/pages/homePage.jsx
@@ -1,56 +1,60 @@
-import "./styles/homepage.css"
-
-
+import "./styles/homepage.css";
 
 const Home = () => {
-    return (  
-        <div>
-            
-  <div className="img"></div>
-  <div className="right">
-    <div className="title">LocalEyes</div>
-    <div className="sub_title"><p>Get involved, stay informed,</p>
-      Make a difference</div>
-    <div className="btns">
-      <button>Join Us</button>
-    </div>  
-</div>
+  return (
+    <div>
+      <div className="img"></div>
+      <div className="right">
+        <div className="title">LocalEyes</div>
+        <div className="sub_title">
+          <p>Get involved, stay informed,</p>
+          Make a difference
+        </div>
+        <div className="btns">
+          <button>Join Us</button>
+        </div>
+      </div>
 
-<section>
+      <section>
+        <h1> TOP ISSUES TO BE ADDRESSED </h1>
 
-<h1> TOP ISSUES TO BE ADDRESSED </h1>
+        <div className="table">
+          <div className="table-cell">
+            <ul className="leader">
+              <li>
+                <span className="list_num">1</span>
+                <h2>
+                  Potholes<span className="number">9,735</span>
+                </h2>
+              </li>
+              <li>
+                <span className="list_num">2</span>
 
-<div className="table">
-  <div className="table-cell">
-    <ul className="leader">
-      <li>
-        <span className="list_num">1</span>
-        <h2>Potholes<span className="number">9,735</span></h2>
-      </li>
-      <li>
-        <span className="list_num">2</span>
-        
-        <h2>Increasing Robbery<span className="number">8,364</span></h2>
-      </li>
-      <li>
-        <span className="list_num">3</span>
-        
-        <h2>Public Toilet Maintenance <span className="number">7,621</span></h2>
-      </li>
-      <li>
-        <span className="list_num">4</span>
-        
-        <h2>Leaking pipes<span className="number">4,582</span></h2>
-      </li>      
-    </ul>
-  </div>
-</div>
-</section>
+                <h2>
+                  Increasing Robbery<span className="number">8,364</span>
+                </h2>
+              </li>
+              <li>
+                <span className="list_num">3</span>
 
+                <h2>
+                  Public Toilet Maintenance{" "}
+                  <span className="number">7,621</span>
+                </h2>
+              </li>
+              <li>
+                <span className="list_num">4</span>
 
+                <h2>
+                  Leaking pipes<span className="number">4,582</span>
+                </h2>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-</div>
-    );
-}
- 
 export default Home;
